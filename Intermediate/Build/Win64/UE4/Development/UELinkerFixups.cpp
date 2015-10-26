827b7b9869b5bfaa9ed259d75cc9158df9f2771e
hlhr202 <hlhr202@gmail.com>
2015-10-26 13:26:50 +0800

    Fmod package
    make deployment available

--- Intermediate/Build/Win64/UE4/Development/UELinkerFixups.cpp
+++ Intermediate/Build/Win64/UE4/Development/UELinkerFixups.cpp
@@ -69,7 +69,7 @@
 	#define UE_BUILD_DEVELOPMENT 1
 #endif
 #ifndef UE_ENGINE_DIRECTORY
-	#define UE_ENGINE_DIRECTORY ../../../../../../../Program Files/Epic Games/4.9/Engine/
+	#define UE_ENGINE_DIRECTORY ../../../../Program Files/Epic Games/4.9/Engine/
 #endif
 #ifndef UE_PROJECT_NAME
 	#define UE_PROJECT_NAME ShooterGame
